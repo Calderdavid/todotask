5f1dd592be5b2b3557bf08595ab467ee787bfbe9
David Calderon <davidjcr200040@gmail.com>
2022-07-07 13:26:32 -0400

    layout login

--- src/Auth/LoginPage.jsx
+++ src/Auth/LoginPage.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import { Grid, TextField, Typography, Button } from "@mui/material"
 
 export const LoginPage = () => {
+    
     return (
-    <h1>Login Page</h1>
+
+    <Grid
+        container
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+    >
+        <Grid item xs={12}>
+            <Typography variant="h3">Login</Typography>       
+        </Grid>
+        <Grid item xs={12} marginTop={-1}>
+            <Typography variant="body1"> 
+                <span>Please enter your login and password!</span>
+            </Typography>
+        </Grid>
+        <Grid item xs={12}>
+            <TextField 
+                label="Email" 
+                type="email" 
+                placeholder="example@gmail.com" 
+                fullWidth 
+                name="email"
+            />
+        </Grid>
+        <Grid item xs={12} marginTop={-1}>
+            <TextField 
+                label="Password" 
+                type="password" 
+                placeholder="password" 
+                fullWidth 
+                name="password"
+            />
+        </Grid>
+        <Grid item xs={12} marginTop={-1}>
+            <Typography variant="body1"> 
+                <span>Please enter your login and password!</span>
+            </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+            <Button variant="contained">Login</Button>
+        </Grid>
+
+        <Grid item xs={12} >
+            <Typography variant="body1"> 
+                <span>Don't have an account?</span>
+                <a href=""><strong>Sign Up</strong></a>
+            </Typography>
+        </Grid>
+    </Grid>
+
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {TodoTask} from './TodoTask'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TodoTask />
